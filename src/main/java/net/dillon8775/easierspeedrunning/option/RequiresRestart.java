ad65197d029f0a1cc8390f7b6d5877dd343ee8d5
Dillon <79867677+Dillon8775@users.noreply.github.com>
2024-02-25 16:35:08 -0500

    Delete src directory

--- src/main/java/net/dillon8775/easierspeedrunning/option/RequiresRestart.java
+++
@@ -1,8 +0,0 @@
-package net.dillon8775.easierspeedrunning.option;
-
-/**
- * Specifies if a {@code easier speedrunning option} requires a restart for the option to take full effect.
- */
-public @interface RequiresRestart {
-    boolean value();
-}
